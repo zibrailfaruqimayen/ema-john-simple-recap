10e595d377a825677809e1b8a007edb8e2954b41
zibrailfaruqimayen <zibrail.faruqi@gmail.com>
2023-04-05 03:46:17 +0600

    order review section styleing

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,44 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Shop from "./Component/Shop/Shop";
+import Home from "./Component/Layout/Home";
+import Orders from "./Component/Orders/Orders";
+import Inventory from "./Component/Inventory/Inventory";
+import Login from "./Component/Login/Login";
+import cartProductsLoader from "./Loaders/cartProductsLoader";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home></Home>,
+    children: [
+      {
+        path: "/",
+        element: <Shop></Shop>,
+      },
+      {
+        path: "orders",
+        element: <Orders></Orders>,
+        loader: cartProductsLoader,
+      },
+      {
+        path: "inventory",
+        element: <Inventory></Inventory>,
+      },
+      {
+        path: "login",
+        element: <Login></Login>,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
