526fafb7a979b38c306cf32b91ee3f822f6164d7
zibrailfaruqimayen <zibrail.faruqi@gmail.com>
2023-04-05 04:08:26 +0600

    order review section styleing

--- src/Component/Orders/Orders.jsx
+++ src/Component/Orders/Orders.jsx
@@ -1,18 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Orders.css";
 import Cart from "../Cart/Cart";
 import { useLoaderData } from "react-router-dom";
 import ReviewItem from "../ReviewItem/ReviewItem";
 
 const Orders = () => {
-  const cart = useLoaderData();
-  console.log(cart);
+  const saveCart = useLoaderData();
+  const [cart, setCart] = useState(saveCart);
+
+  const handleRemoveFromCart = (id) => {
+    const remaining = cart.filter((product) => product.id !== id);
+    setCart(remaining);
+  };
 
   return (
     <div className="shop-container">
       <div className="review-container">
         {cart.map((product) => (
-          <ReviewItem key={product.id} product={product}></ReviewItem>
+          <ReviewItem
+            key={product.id}
+            product={product}
+            handleRemoveFromCart={handleRemoveFromCart}
+          ></ReviewItem>
         ))}
       </div>
       <div className="cart-container">
